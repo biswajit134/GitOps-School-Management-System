31c0262efc0d3175b2ef378075712e51fd60cb9a
biswajit134 <biswajithazra133@gmail.com>
2026-04-03 16:54:01 +0530

    CI/CD ADD TF_APPLY.yml

--- terraform/kubernetes-config/main.tf
+++ terraform/kubernetes-config/main.tf
@@ -18,7 +18,7 @@ terraform {
 }
 
 data "azurerm_kubernetes_cluster" "default" {
-  depends_on          = [module.aks-cluster] # refresh cluster state before reading
+  # depends_on          = [module.aks-cluster] # refresh cluster state before reading
   name                = var.cluster_name
   resource_group_name = var.cluster_name
 }
