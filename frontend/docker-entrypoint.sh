db6465449aef25ddbb94213a541963d4a2d9c20e
biswajit134 <biswajithazra133@gmail.com>
2026-05-01 02:49:10 +0530

    CHANGE frontend/Dockerfile

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 echo "Injecting runtime environment variables..."
  
 # Replace the build-time placeholder with the actual runtime value
-# Requires the Dockerfile to bake in REACT_APP_BACKEND_URL=__REACT_APP_BACKEND_URL__
+# Requires the Dockerfile to bake in REACT_APP_BASE_URL=__REACT_APP_BASE_URL__
 find /usr/share/nginx/html/static/js -name '*.js' | xargs sed -i \
-  "s|__REACT_APP_BACKEND_URL__|${REACT_APP_BASE_URL}|g"
+  "s|__REACT_APP_BASE_URL__|${REACT_APP_BASE_URL}|g"
  
-echo "REACT_APP_BACKEND_URL set to: ${REACT_APP_BASE_URL}"
+echo "REACT_APP_BASE_URL set to: ${REACT_APP_BASE_URL}"
  
 exec nginx -g 'daemon off;'
  
